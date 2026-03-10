990c9a86b0494e5a0d722c9e9968a4226c77b940
soumojit <soumojidebnath07610761@gmail.com>
2026-03-10 15:55:55 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="soumojit debnath",
+    author_email="soumojitdebnath07610761@gmail.com",
+    packages=find_packages()
+)
